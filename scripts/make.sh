3edc8d8d17e6d2e140e4922af7290031e4a598b0
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2022-11-23 17:15:13 -0800

    Update make.sh

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,11 +1,9 @@
-cd ../src/
+#!/usr/bin/env bash
+cd src/
 echo -e 'Fetching data ...\n'
-Rscript fetch.R ES 'Autonomous community' 'Autonomous city'
-echo -e '\nMapping regions to administrative regions of Spain ...\n'
-Rscript locate.R fetched/ES/ES-latest-estimate.csv located/ES/ES-loc.csv
-cp located/ES/ES-loc.csv located/ES/ES-geo.csv
-echo -e '\nPreparing data for CARTO ...\n'
-Rscript transform.R located/ES/ES-geo.csv
+Rscript fetch.R US
+echo -e '\nLocating regions ...\n'
+Rscript locate.R fetched/US/US-latest-estimate.csv
 echo -e '\nGenerating map ...\n'
 python map.py
-echo -e '\nDone! A map should be generated in your CARTO dashboard.'
+echo -e '\nDone! A map should be generated in an open browser window.'
