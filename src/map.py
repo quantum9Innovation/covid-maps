884e95ef1f2a257c963883a182247804d748e62d
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2022-12-11 12:29:20 -0800

    Add South Africa (ZA)

--- src/map.py
+++ src/map.py
@@ -21,7 +21,7 @@ countrycode = sys.argv[1]
 country_lower = countrycode.lower()
 
 # validate arguments
-COUNTRIES = ['CL', 'DE', 'ES', 'FR', 'GB', 'GR', 'US']
+COUNTRIES = ['CL', 'DE', 'ES', 'FR', 'GB', 'GR', 'US', 'ZA']
 if countrycode not in COUNTRIES and countrycode != 'all':
     print(
         'ERROR: Invalid country code. '
